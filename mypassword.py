5b433994975813fa460c993b80e4a386fc8cf8d5
Joshua Tallman <joshuatallman78@gmail.com>
2020-07-06 17:57:28 +0300

    Assignment - 1 (if-Statements)

---
+++ mypassword.py
@@ -0,0 +1,8 @@
+myname = "Joshua"
+
+get_name = input("Please Enter Your Name: ")
+
+if myname == get_name:
+    print(f"Hello, {get_name}! The password is : Deneme.123")
+else:
+    print(f"Hello, {get_name}! See you later.")
